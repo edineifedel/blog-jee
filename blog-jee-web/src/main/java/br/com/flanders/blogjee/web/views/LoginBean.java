8958ab452c9c149c1505a5a0e4b00d297b97690d
Edinei Fedel <neigroove@gmail.com>
2012-08-24 23:43:09 -0300

    Static images

---
+++ blog-jee-web/src/main/java/br/com/flanders/blogjee/web/views/LoginBean.java
@@ -0,0 +1,33 @@
+package br.com.flanders.blogjee.web.views;
+
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import br.com.flanders.blogjee.entities.User;
+
+@ManagedBean
+@RequestScoped
+public class LoginBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Inject
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String login() {
+		FacesContext.getCurrentInstance().addMessage("pwd",
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Usu‡rio/Senha incorreto", null));
+		return null;
+		// return "/blog/posts/list";
+	}
+}
---
+++ blog-jee-web/src/main/java/br/com/flanders/blogjee/web/views/PostBean.java
@@ -0,0 +1,21 @@
+package br.com.flanders.blogjee.web.views;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import br.com.flanders.blogjee.entities.Post;
+
+@ManagedBean
+@RequestScoped
+public class PostBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private static List<Post> posts = Arrays.asList(new Post[] { new Post("Meu primeiro post", "blahblah"),
+			new Post("Meu segundo post", "blehbleh") });
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+}
